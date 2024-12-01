7779475be11199d2029cff76bd57a0c769b254fa
Piotr Grzywna <Piotr.Grzywna@pl.ibm.com>
2024-12-01 18:49:03 +0100

    day1 - ex 1 example

---
+++ day01/day01_1_convert_to_matrix.py
@@ -0,0 +1,29 @@
+def read_fine(path2file):
+    with open(path2file, 'r', encoding="utf8") as file:
+        lines = file.readlines()
+    return lines
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(' '.join(str(row)))
+
+def process_line(line):
+    pass
+
+def main():
+    matrix = []
+    path2file = './input/input_day01example'
+    lines = read_fine(path2file)
+    lines = [line.strip() for line in lines]
+    print("Lines = ", lines)
+
+    for line in lines:
+        print(" > line = ", line)
+        # processed_line = process_line(line)
+        row = list(map(int, line.split()))
+        matrix.append(row)
+
+    print('Result matrix =\n', print_matrix(matrix))
+
+if __name__ == "__main__":
+    main()
