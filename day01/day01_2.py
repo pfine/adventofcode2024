09df8696124908dfbde773da32ec397fc4cedf9d
Piotr Grzywna <Piotr.Grzywna@pl.ibm.com>
2024-12-01 19:16:52 +0100

    day1 - ex 2 done

--- day01/day01_2.py
+++ day01/day01_2.py
@@ -8,7 +8,7 @@ def process_line(line):
 
 def main():
     matrix = [[], []]
-    path2file = './input/input_day01'
+    path2file = './input/input_day01_2'
     lines = read_fine(path2file)
     lines = [line.strip() for line in lines]
     print("Lines = ", lines)
@@ -23,27 +23,14 @@ def main():
 
     print("matrix =", matrix)
 
-    # sort from lowest
-    list_1 = matrix[0]
-    print(" > list_1 =", list_1)
-    list_1.sort() # sort in plase, result None
-    print(" > list_1 =", list_1)
-    # print(" > list_1 =", list_1, type(list_1), list_1.sort(reverse=False), list_1, sorted(list_1))
-    list_2 = matrix[1]
-    print(" > list_2 =", list_2)
-    list_2.sort() # sort in place, result None
-    print(" > list_2 =", list_2)
-    # print(" > list_2 =", list_2, type(list_2), list_2.sort(reverse=False), list_2, sorted(list_2))
-
-    print("matrix =", matrix)
-
-    distance = 0
+    score = 0
     # mesure distance
     for i in range(len(matrix[0])):
-        distance = distance + abs(matrix[0][i] - matrix[1][i])
-        print('   > summed distance between', matrix[0][i], "and", matrix[1][i], "is", abs(matrix[0][i] - matrix[1][i]))
+        occurres_in_list2_times = matrix[1].count(matrix[0][i])
+        score = score + matrix[0][i] * occurres_in_list2_times
+        # print('   > Similarity score between element ', matrix[0][i], "and", matrix[1], "is", matrix[0][i] * occurres_in_list2_times)
 
-    print('Summed distance is', distance)
+    print('Similarity score is', score)
 
 
 if __name__ == "__main__":
